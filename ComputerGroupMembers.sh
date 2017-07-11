baa10c1e98ac1300d27ffe38a609dc80c7b6dfad
Brian Bocklett <brian.bocklett@gmail.com>
2017-07-11 01:19:25 -0400

    API script i made to help someone get an idea of how to get computer group members listed out, will likely adjust to output to csv, or something more useful than a txt.

---
+++ ComputerGroupMembers.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+##################################################################
+# 
+##################################################################
+
+## Functions ####################################################
+
+function getGroupMembers() {
+	ComputerNames=$(/usr/bin/curl -s -u "${apiuser}:${apipass}" "${jss}/JSSResource/computergroups/name/${groupName}" | /usr/bin/xpath "//computer_group/computers/computer/name/text() " 2>/dev/null)
+}
+
+## Variables ####################################################
+jss="$(osascript -e 'Tell application "System Events" to display dialog "Enter JSS URL: " default answer ""' -e 'text returned of result' 2>/dev/null)"
+apiuser="$(osascript -e 'Tell application "System Events" to display dialog "Enter API Username: " default answer ""' -e 'text returned of result' 2>/dev/null)"
+apipass="$(osascript -e 'Tell application "System Events" to display dialog "Enter API Password: " with hidden answer default answer ""' -e 'text returned of result' 2>/dev/null)"
+groupName="$(osascript -e 'Tell application "System Events" to display dialog "Enter Group Name: " default answer ""' -e 'text returned of result' 2>/dev/null)"
+
+## Work Area ####################################################
+
+getGroupMembers
+
+if [[ "$ComputerNames" != "" ]]; then 
+touch /Users/Shared/"${groupName}".txt
+
+echo "================================"
+echo "Group Name: $groupName"
+echo "================================"
+echo "Group Members: "
+echo "--------------------------------"
+echo "$ComputerNames"
+
+fi > "/Users/Shared/${groupName}.txt"
+
+if [[ -a /Users/Shared/"${groupName}".txt ]]; then
+osascript <<EOF
+ Tell application "System Events" to display dialog "Computer Group Information: 
+ ----------------------------------------------------
+Please check log file for results.
+/Users/Shared/${groupName}.txt"
+EOF
+else
+osascript <<EOF
+ Tell application "System Events" to display dialog "Computer Group Information: 
+ ----------------------------------------------------
+Text File Not Created. Please contat your jamf Administrator for more information"
+EOF
+fi
+
+exit 0
